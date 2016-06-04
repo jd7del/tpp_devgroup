0a66d82029b1e9e04c6b6f38e34d4a32bacecbb2
Jason Delgado M <jd7.del@gmail.com>
2016-06-04 18:53:30 -0500

    se agrego el modulo volley

--- app/src/main/java/com/projectsandroid/www/tupromo/controller/activities/MainActivity.java
+++ app/src/main/java/com/projectsandroid/www/tupromo/controller/activities/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity
 
     private Boolean exit = false;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
