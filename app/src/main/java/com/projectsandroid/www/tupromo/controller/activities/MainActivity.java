2e388ef8d2bd9741991bf9bd4bb857979f20718c
Jason Delgado M <jd7.del@gmail.com>
2016-06-05 14:22:02 -0500

    sida_var icons

--- app/src/main/java/com/projectsandroid/www/tupromo/controller/activities/MainActivity.java
+++ app/src/main/java/com/projectsandroid/www/tupromo/controller/activities/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //fabButton
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +44,7 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        //drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
--- app/src/main/java/com/projectsandroid/www/tupromo/controller/adapters/Adp_promos.java
+++ app/src/main/java/com/projectsandroid/www/tupromo/controller/adapters/Adp_promos.java
@@ -4,4 +4,5 @@ package com.projectsandroid.www.tupromo.controller.adapters;
  * Created by Jason Delgado M on 04/06/2016.
  */
 public class Adp_promos {
+
 }
